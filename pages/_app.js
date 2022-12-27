774976e5c32e81c1a21fe3527b8066d4c4c3c19c
sapeol <sudeep.kuchara@protonmail.com>
2022-12-27 17:22:46 +0530

    intial blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import
+{
+  Open_Sans
+} from '@next/font/google';
+const sans = Open_Sans( { subsets: [ 'latin' ] } );
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App ( { Component, pageProps } )
+{
+  return <main className={ sans.className }>
+    <Component { ...pageProps } />;
+  </main>;
 }
